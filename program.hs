08b86724d83e2e46142a2c554aeaa7f35b1deaf9
fbmlj <lucastavasousa@gmail.com>
2020-10-12 18:45:05 -0300

    funçao atomica funcionando

--- program.hs
+++ program.hs
@@ -27,17 +27,40 @@ binToBool :: [Integer] -> Int -> Bool
 binToBool list pos = if (/=) (list !! pos) 1 then True else False
 
 
+solveAtomicExpress :: [String] ->  Bool
+solveAtomicExpress express = case (express !! 1) of
+    ("->") ->  ifAtomicExpress(express)
+    ("&") -> andAtomicExpress(express)
+    ("|") -> orAtomicExpress(express)
+  
 
 
+ifAtomicExpress :: [String] -> Bool
+ifAtomicExpress express = if ((express !! 0) == "1" && (express !! 2) == "0") then  False else  True
 
+orAtomicExpress :: [String] -> Bool
+orAtomicExpress express = if ((express !! 0) == "1" || (express !! 2) == "1") then True else False
+
+andAtomicExpress :: [String] -> Bool
+andAtomicExpress express = if ((express !! 0) == "1" && (express !! 2) == "1") then True else False
 main = do
     -- print((split "oi gente") !! 0)
-    putStrLn("Escreva as variaveis da tabela verdade")
-    input <- getLine
-    let variavel = split input
-    putStrLn("escreva a expressão da tabela verdade")
-    fuction <- getLine
+    --------------------------------------
+    -- Bloco de Codigo de Teste
+    input <- getLine;
 
-    print(toBin 8)
-    print(length (toBin 8))
-    print(equalizeLen (toBin 8) 10 )
+    print(solveAtomicExpress(split(input)))
+    putStr("oi")
+
+
+    -------------------------------------
+    --- Bloco de Codigo funcional
+    -- putStrLn("Escreva as variaveis da tabela verdade")
+    -- input <- getLine
+    -- let variavel = split input
+    -- putStrLn("escreva a expressão da tabela verdade")
+    -- fuction <- getLine
+
+    -- print(toBin 8)
+    -- print(length (toBin 8))
+    -- print(equalizeLen (toBin 8) 10 )
