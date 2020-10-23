fc7e86fe74cbf2f83ac145c5634799fa8aceecd9
fbmlj <lucastavasousa@gmail.com>
2020-10-23 19:39:11 -0300

    codigo esta começando fazer sentido

--- tabela_verdade.hs
+++ tabela_verdade.hs
@@ -9,18 +9,43 @@ findChar exp char id = if (exp !! id) == char then True else if (id + 1) == (len
 orController :: String -> [String]
 orController exp = if (findChar exp '(' 0) then [""] else ["testando"]
 
+-- função responsavel para separa as subformulas do resto da formula
+find_subformula :: String -> Int -> (String, Int)
+find_subformula exp id = 
+    -- fica pulando para o proximo id até encontrar um caracter valido
+    if (exp !! id) == ' ' || (exp !! id) == '>' then find_subformula exp (id + 1)
+    -- encontra subformulas que estão dentro de parenteses
+    else if (exp !! id) == '(' then do
+        let var = (find_subformula_aux exp (id + 1) 1)
+        (slice id var exp, var)
+    -- no caso de ser uma negação de uma subformula atomica ou a propria subformula atomica
+    else (" ", 1)
 
+-- auxilia a função acima
+find_subformula_aux :: String -> Int -> Int -> Int
+find_subformula_aux exp current parenteses = if parenteses == 0 then (current - 1) 
+    else if exp !! current == '('  then find_subformula_aux exp (current + 1) (parenteses + 1) 
+    else if exp !! current == ')' then find_subformula_aux exp (current + 1) (parenteses - 1)
+    else find_subformula_aux exp (current + 1) parenteses
+
+
+-- verifica o operador que será utilizado para resolver a formula
 operationController :: String -> [String]
 operationController express = case (express !! 0) of
-    ('|') -> [""]
+    ('|') -> do 
+        let (first, id) = find_subformula express 1
+        let (second, id2) = find_subformula express (id + 1)
+        [first, second]
     (_) -> error "Formato da equação não condiz com o esperado(2)"
 
 
+
+-- verifica a integridade da formula ou subformula e retorna erro se não estiver de acordo com a sintaxe
 expressController :: String -> [String]
 expressController attr = if (attr !! 0) == '(' && (attr !! (length attr -1)) == ')' then operationController(slice 1 (length attr -2) attr) else error "Formato da equação não condiz com o esperado(1)"
 
 main = do
     input <- getLine
     
-    let response = expressController(input)
-    print(response)
+    let response= expressController input
+    print(response) 
